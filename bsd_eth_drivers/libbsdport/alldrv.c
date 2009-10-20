e221bff4ad0ae79efa168c64f5a6da44da26c6e6
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-10-20 13:31:47 +0000

    2009-10-20	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* libbsdport/alldrv.c: Revert.

--- bsd_eth_drivers/libbsdport/alldrv.c
+++ bsd_eth_drivers/libbsdport/alldrv.c
@@ -15,7 +15,6 @@ driver_t *libbsdport_netdriver_table_all[] = {
 
 driver_t libbsdport_null_driver = {0};
 
-#if 0
 extern driver_t libbsdport_em_driver
 	__attribute__((weak,alias("libbsdport_null_driver")));
 extern driver_t libbsdport_pcn_driver
@@ -30,7 +29,6 @@ extern driver_t libbsdport_re_driver
 	__attribute__((weak,alias("libbsdport_null_driver")));
 extern driver_t libbsdport_rl_driver
 	__attribute__((weak,alias("libbsdport_null_driver")));
-#endif
 
 
 /* weak alias defaults to a table that includes
